be6cbfd0e24bc0c8ed3c22118206e5c3bf6d7ac4
Marius Engen <marius@edyou.no>
2022-04-28 14:27:42 +0200

    Add solution Assignment 4

--- src/SSD/ssd/modeling/backbones/basic.py
+++ src/SSD/ssd/modeling/backbones/basic.py
@@ -20,6 +20,51 @@ class BasicModel(torch.nn.Module):
         super().__init__()
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
+        self.feature_extractor = torch.nn.Sequential(
+            torch.nn.Conv2d(image_channels, 32, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2,2), # 150x150 out
+            torch.nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2,2), # 75 x 75 out
+            torch.nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            # Use ceil mode to get 75/2 to ouput 38
+            torch.nn.Conv2d(64, output_channels[0], kernel_size=3, padding=1, stride=2),
+            torch.nn.ReLU(),
+        ) 
+        self.additional_layers = torch.nn.ModuleList([
+            torch.nn.Sequential( # 19 x 19 out
+                torch.nn.Conv2d(output_channels[0], 128, kernel_size=3, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(128, output_channels[1], kernel_size=3, padding=1, stride=2),
+                torch.nn.ReLU(),
+            ),
+            torch.nn.Sequential( # 10x10 out
+                torch.nn.Conv2d(output_channels[1], 256, kernel_size=3, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(256, output_channels[2], kernel_size=3, padding=1, stride=2),
+                torch.nn.ReLU(),
+            ),
+            torch.nn.Sequential( # 5 x 5 out
+                torch.nn.Conv2d(output_channels[2], 128, kernel_size=3, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(128, output_channels[3], kernel_size=3, padding=1, stride=2),
+                torch.nn.ReLU(),
+            ),
+            torch.nn.Sequential( # 3 x 3 out
+                torch.nn.Conv2d(output_channels[3], 128, kernel_size=3, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(128, output_channels[4], kernel_size=3, stride=2, padding=1),
+                torch.nn.ReLU(),
+            ),
+            torch.nn.Sequential( # 1 x 1 out
+                torch.nn.Conv2d(output_channels[4], 128, kernel_size=3, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(128, output_channels[5], kernel_size=3),
+                torch.nn.ReLU(),
+            ),
+        ])
 
     def forward(self, x):
         """
@@ -35,6 +80,11 @@ class BasicModel(torch.nn.Module):
             shape(-1, output_channels[0], 38, 38),
         """
         out_features = []
+        x = self.feature_extractor(x)
+        out_features.append(x)
+        for additional_layer in self.additional_layers.children():
+            x = additional_layer(x)
+            out_features.append(x)
         for idx, feature in enumerate(out_features):
             out_channel = self.out_channels[idx]
             h, w = self.output_feature_shape[idx]
@@ -45,3 +95,79 @@ class BasicModel(torch.nn.Module):
             f"Expected that the length of the outputted features to be: {len(self.output_feature_shape)}, but it was: {len(out_features)}"
         return tuple(out_features)
 
+
+class BasicModelExtended(BasicModel): 
+
+    def __init__(self, 
+            output_channels: List[int], 
+            image_channels: int, 
+            output_feature_sizes: List[Tuple[int]]):
+        super().__init__(output_channels, image_channels, output_feature_sizes)
+        self.output_channels = output_channels
+        self.feature_extractor = torch.nn.Sequential(
+            torch.nn.Conv2d(image_channels, 32, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.LeakyReLU(0.2),
+            torch.nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.LeakyReLU(0.2),
+            
+            torch.nn.MaxPool2d(2,2), # 150x150 out
+            torch.nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(128),
+            torch.nn.LeakyReLU(0.2),
+            torch.nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(128),
+            torch.nn.LeakyReLU(0.2),
+            torch.nn.MaxPool2d(2,2), # 75 x 75 out
+            torch.nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(256),
+            torch.nn.LeakyReLU(0.2),
+            torch.nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(512),
+            torch.nn.LeakyReLU(0.2),
+            # Use ceil mode to get 75/2 to ouput 38
+            torch.nn.Conv2d(512, output_channels[0], kernel_size=3, padding=1, stride=2),
+        )
+        self.additional_layers = torch.nn.ModuleList([
+            torch.nn.Sequential( # 19 x 19 out
+                torch.nn.BatchNorm2d(output_channels[0]),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(output_channels[0], 512, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(512),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(512, output_channels[1], kernel_size=3, padding=1, stride=2),
+            ),
+            torch.nn.Sequential( # 10x10 out
+                torch.nn.BatchNorm2d(output_channels[1]),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(output_channels[1], 256, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(256),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(256, output_channels[2], kernel_size=3, padding=1, stride=2),
+            ),
+            torch.nn.Sequential( # 5 x 5 out
+                torch.nn.BatchNorm2d(output_channels[2]),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(output_channels[2], 128, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(128),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(128, output_channels[3], kernel_size=3, padding=1, stride=2),
+            ),
+            torch.nn.Sequential( # 3 x 3 out
+                torch.nn.BatchNorm2d(output_channels[3]),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(output_channels[3], 128, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(128),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(128, output_channels[4], kernel_size=3, stride=2, padding=1),
+            ),
+            torch.nn.Sequential( # 1 x 1 out
+                torch.nn.BatchNorm2d(output_channels[4]),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(output_channels[4], 128, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(128),
+                torch.nn.LeakyReLU(0.2),
+                torch.nn.Conv2d(128, output_channels[5], kernel_size=3),
+            ),
+        ])
--- src/task2/task2.py
+++ src/task2/task2.py
@@ -5,22 +5,33 @@ from tools import read_predicted_boxes, read_ground_truth_boxes
 
 def calculate_iou(prediction_box, gt_box):
     """Calculate intersection over union of single predicted and ground truth box.
-
     Args:
         prediction_box (np.array of floats): location of predicted object as
             [xmin, ymin, xmax, ymax]
         gt_box (np.array of floats): location of ground truth object as
             [xmin, ymin, xmax, ymax]
-
         returns:
             float: value of the intersection of union for the two boxes.
     """
-    # YOUR CODE HERE
+    
+    x1_t, y1_t, x2_t, y2_t = gt_box 
+    x1_p, y1_p, x2_p, y2_p = prediction_box 
+    if x2_t < x1_p or x2_p < x1_t or y2_t < y1_p or y2_p < y1_t:
+        return 0.0
 
     # Compute intersection
+    x1i = max(x1_t, x1_p)
+    x2i = min(x2_t, x2_p)
+    y1i = max(y1_t, y1_p)
+    y2i = min(y2_t, y2_p)
+    intersection = (x2i - x1i) * (y2i - y1i)
 
     # Compute union
+    pred_area = (x2_p - x1_p) * (y2_p - y1_p)
+    gt_area = (x2_t - x1_t) * (y2_t - y1_t)
+    union = pred_area + gt_area - intersection
     iou = 0
+    iou = intersection / union
     assert iou >= 0 and iou <= 1
     return iou
 
@@ -28,7 +39,6 @@ def calculate_iou(prediction_box, gt_box):
 def calculate_precision(num_tp, num_fp, num_fn):
     """ Calculates the precision for the given parameters.
         Returns 1 if num_tp + num_fp = 0
-
     Args:
         num_tp (float): number of true positives
         num_fp (float): number of false positives
@@ -36,7 +46,9 @@ def calculate_precision(num_tp, num_fp, num_fn):
     Returns:
         float: value of precision
     """
-    raise NotImplementedError
+    if (num_tp + num_fp) == 0:
+        return 1
+    return num_tp / (num_fp + num_tp)
 
 
 def calculate_recall(num_tp, num_fp, num_fn):
@@ -49,15 +61,16 @@ def calculate_recall(num_tp, num_fp, num_fn):
     Returns:
         float: value of recall
     """
-    raise NotImplementedError
+    denominator = num_tp + num_fn
+    if denominator == 0:
+        return 0
+    return num_tp / denominator
 
 
 def get_all_box_matches(prediction_boxes, gt_boxes, iou_threshold):
     """Finds all possible matches for the predicted boxes to the ground truth boxes.
         No bounding box can have more than one match.
-
         Remember: Matching of bounding boxes should be done with decreasing IoU order!
-
     Args:
         prediction_boxes: (np.array of floats): list of predicted bounding boxes
             shape: [number of predicted boxes, 4].
@@ -72,15 +85,42 @@ def get_all_box_matches(prediction_boxes, gt_boxes, iou_threshold):
             objects with shape: [number of box matches, 4].
             Each row includes [xmin, ymin, xmax, ymax]
     """
+    ious = []
+    indices = []
     # Find all possible matches with a IoU >= iou threshold
+    for pred_idx, pred_box in enumerate(prediction_boxes):
+        for gt_idx, gt_box in enumerate(gt_boxes):
+            iou = calculate_iou(pred_box, gt_box)
+            if iou >= iou_threshold:
+                ious.append(iou)
+                indices.append((pred_idx, gt_idx))
 
+    ious = np.array(ious)
+    indices = np.array(indices)
+    if indices.size == 0:
+        return np.array([]), np.array([])
+    assert indices.shape[1] == 2
 
     # Sort all matches on IoU in descending order
+    sorted_idx = np.argsort(ious)[::-1]
+    ious = ious[sorted_idx]
+    indices = indices[sorted_idx]
 
     # Find all matches with the highest IoU threshold
+    seen_prediction_boxes = np.zeros(len(prediction_boxes))
+    seen_gt_boxes = np.zeros(len(gt_boxes))
+    final_prediction_boxes = []
+    final_gt_boxes = []
 
+    for (pred_idx, gt_idx) in indices:
+        if seen_prediction_boxes[pred_idx] == 0 and seen_gt_boxes[gt_idx] == 0:
+            final_prediction_boxes.append(prediction_boxes[pred_idx])
+            final_gt_boxes.append(gt_boxes[gt_idx])
 
+            seen_prediction_boxes[pred_idx] = 1
+            seen_gt_boxes[gt_idx] = 1
 
+    return np.array(final_prediction_boxes), np.array(final_gt_boxes)
     return np.array([]), np.array([])
 
 
@@ -89,7 +129,6 @@ def calculate_individual_image_result(prediction_boxes, gt_boxes, iou_threshold)
        calculates true positives, false positives and false negatives
        for a single image.
        NB: prediction_boxes and gt_boxes are not matched!
-
     Args:
         prediction_boxes: (np.array of floats): list of predicted bounding boxes
             shape: [number of predicted boxes, 4].
@@ -101,8 +140,13 @@ def calculate_individual_image_result(prediction_boxes, gt_boxes, iou_threshold)
         dict: containing true positives, false positives, true negatives, false negatives
             {"true_pos": int, "false_pos": int, false_neg": int}
     """
+    matched_pred_boxes, matched_gt_boxes = get_all_box_matches( 
+        prediction_boxes, gt_boxes, iou_threshold)
 
-    raise NotImplementedError
+    num_tp = len(matched_gt_boxes)
+    num_fp = len(prediction_boxes) - num_tp
+    num_fn = len(gt_boxes) - num_tp
+    return {"true_pos": num_tp, "false_pos": num_fp, "false_neg": num_fn}
 
 
 def calculate_precision_recall_all_images(
@@ -111,7 +155,6 @@ def calculate_precision_recall_all_images(
        calculates recall and precision over all images
        for a single image.
        NB: all_prediction_boxes and all_gt_boxes are not matched!
-
     Args:
         all_prediction_boxes: (list of np.array of floats): each element in the list
             is a np.array containing all predicted bounding boxes for the given image
@@ -124,7 +167,19 @@ def calculate_precision_recall_all_images(
     Returns:
         tuple: (precision, recall). Both float.
     """
-    raise NotImplementedError
+    true_pos = 0
+    false_pos = 0
+    false_neg = 0
+    for idx in range(len(all_prediction_boxes)):
+        pbox = all_prediction_boxes[idx]
+        gtbox = all_gt_boxes[idx]
+        res = calculate_individual_image_result(pbox, gtbox, iou_threshold)
+        true_pos += res["true_pos"]
+        false_pos += res["false_pos"]
+        false_neg += res["false_neg"]
+    precision = calculate_precision(true_pos, false_pos, false_neg)
+    recall = calculate_recall(true_pos, false_pos, false_neg)
+    return precision, recall 
 
 
 def get_precision_recall_curve(
@@ -133,9 +188,7 @@ def get_precision_recall_curve(
     """Given a set of prediction boxes and ground truth boxes for all images,
        calculates the recall-precision curve over all images.
        for a single image.
-
        NB: all_prediction_boxes and all_gt_boxes are not matched!
-
     Args:
         all_prediction_boxes: (list of np.array of floats): each element in the list
             is a np.array containing all predicted bounding boxes for the given image
@@ -148,7 +201,6 @@ def get_precision_recall_curve(
         scores: (list of np.array of floats): each element in the list
             is a np.array containting the confidence score for each of the
             predicted bounding box. Shape: [number of predicted boxes]
-
             E.g: score[0][1] is the confidence score for a predicted bounding box 1 in image 0.
     Returns:
         precisions, recalls: two np.ndarray with same shape.
@@ -160,6 +212,18 @@ def get_precision_recall_curve(
 
     precisions = [] 
     recalls = []
+    for confidence_thr in confidence_thresholds:
+        temp_pred_box = []
+        for image_idx in range(len(all_prediction_boxes)):
+            pred_boxes = all_prediction_boxes[image_idx]
+            scores = confidence_scores[image_idx]
+            prune_mask = scores >= confidence_thr
+            temp_pred_box.append(pred_boxes[prune_mask])
+        precision, recall = calculate_precision_recall_all_images(
+            temp_pred_box, all_gt_boxes, iou_threshold
+        )
+        precisions.append(precision)
+        recalls.append(recall)
     return np.array(precisions), np.array(recalls)
 
 
@@ -167,7 +231,6 @@ def plot_precision_recall_curve(precisions, recalls):
     """Plots the precision recall curve.
         Save the figure to precision_recall_curve.png:
         'plt.savefig("precision_recall_curve.png")'
-
     Args:
         precisions: (np.array of floats) length of N
         recalls: (np.array of floats) length of N
@@ -186,7 +249,6 @@ def plot_precision_recall_curve(precisions, recalls):
 def calculate_mean_average_precision(precisions, recalls):
     """ Given a precision recall curve, calculates the mean average
         precision.
-
     Args:
         precisions: (np.array of floats) length of N
         recalls: (np.array of floats) length of N
@@ -196,14 +258,22 @@ def calculate_mean_average_precision(precisions, recalls):
     # Calculate the mean average precision given these recall levels.
     recall_levels = np.linspace(0, 1.0, 11)
     # YOUR CODE HERE
+    final_precisions = []
+    for recall_level in recall_levels:
+        precision = precisions[recalls >= recall_level]
+        if precision.size == 0:
+            precision = 0
+        else:
+            precision = max(precision)
+        final_precisions.append(precision)
     average_precision = 0
+    average_precision = np.mean(final_precisions)
     return average_precision
 
 
 def mean_average_precision(ground_truth_boxes, predicted_boxes):
     """ Calculates the mean average precision over the given dataset
         with IoU threshold of 0.5
-
     Args:
         ground_truth_boxes: (dict)
         {
