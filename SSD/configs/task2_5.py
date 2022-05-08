c3330d707ceaf0520e9141de21e841dc7f1d8957
Marius Engen <marius@edyou.no>
2022-05-08 09:13:02 +0200

    Add missing anchors in config

--- SSD/configs/task2_5.py
+++ SSD/configs/task2_5.py
@@ -10,11 +10,12 @@ from .task2_4 import (
     train_cpu_transform,
     val_cpu_transform,
     gpu_transform,
-    label_map
+    label_map,
+    anchors
 )
 from .utils import get_dataset_dir
 
-# Run on updated dataset
+# Run on updated dataset for Task 2.5
 data_train.dataset.img_folder = get_dataset_dir("tdt4265_2022_updated")
 data_train.dataset.annotation_file = get_dataset_dir("tdt4265_2022_updated/train_annotations.json")
 data_val.dataset.img_folder = get_dataset_dir("tdt4265_2022_updated")
