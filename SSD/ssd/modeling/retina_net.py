4d9bb5e1f331a90d4f93636420c0088a6a75f757
Marius Engen <marius@edyou.no>
2022-05-08 06:41:07 +0200

    ToTensorn da vet

--- SSD/ssd/modeling/retina_net.py
+++ SSD/ssd/modeling/retina_net.py
@@ -45,14 +45,14 @@ class RetinaNet(nn.Module):
                     if param.dim() > 1: nn.init.xavier_uniform_(param)
         else:
             # Improved weight initialization for Task 2.3.4.
-            π = .99
+            π = torch.tensor(.99)
             # Initialize all new conv layers except for the final one with bias=0 and a Gaussian weight fill with sigma=0.01.
             for layer in layers[:-1]:
                 for param in layer.parameters():
                     if param.dim() > 1: nn.init.normal_(param, 0, 0.01)
 
             # For the final conv layer of the regression_heads and classification_heads, we set the bias initialization to b = - log ((1 - π) / π).
-            n = self.num_boxes_per_fmap[-1]
+            n = torch.tensor(self.num_boxes_per_fmap[-1])
             bias = torch.ones(n, 1) * torch.log(π * (self.num_classes - 1)/(1 - π)).flatten()  #TODO: test with 1 instead of self.num_classes
             self.classification_heads[-1][-1].bias.data[:n] = bias.flatten().clone()
             self.regression_heads[-1][-1].bias.data[:n] = bias.flatten().clone()
